2c5610a6b7bd34ed5f8d80584d8e01821896bcbc
Amirul <22FTT1493@student.pb.edu.bn>
2024-03-07 15:08:26 +0800

    Add routes for offers, places, and tabs pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,4 +10,16 @@ export const routes: Routes = [
     path: 'auth',
     loadComponent: () => import('./auth/auth.page').then((m) => m.AuthPage),
   },
+  {
+    path: 'offers',
+    loadComponent: () => import('./offers/offers.page').then( m => m.OffersPage)
+  },
+  {
+    path: 'places',
+    loadComponent: () => import('./places/places.page').then( m => m.PlacesPage)
+  },
+  {
+    path: 'tabs',
+    loadComponent: () => import('./tabs/tabs.page').then( m => m.TabsPage)
+  },
 ];
